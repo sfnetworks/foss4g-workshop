40f1c96a4dcd1676ceac00a04629627aaf192955
loreabad6 <lore.abad6@gmail.com>
2022-08-18 23:36:07 +0200

    Regenerate Florence network

--- scripts/get_florence_data.R
+++ scripts/get_florence_data.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(osmextract)
+library(osmdata)
 library(sf)
 
 # Obtain Florence highways from OSM
@@ -11,24 +12,31 @@ oe_download(
   file_size = florence_info$file_size,
   download_directory = "data/"
 )
-italy_lines = oe_read(
-  "data/geofabrik_centro-latest.osm.pbf",
-  quiet = FALSE,
-  query = "SELECT highway, geometry FROM 'lines'"
-)
-
+italy_pbf = "data/geofabrik_centro-latest.osm.pbf"
 florence = getbb(
   'Florence, Italy',
   featuretype = "city",
   format_out = "sf_polygon"
 )
+extra_tags_ls =  c("bicycle", "foot", "access",
+                   "surface", "oneway", "maxspeed",
+                   "tunnel", "bridge")
+florence_lines = oe_vectortranslate(
+  italy_pbf,
+  layer = 'lines',
+  extra_tags = extra_tags_ls,
+  boundary = florence,
+  boundary_type = "spat"
+)
+
 
 # Create a LINESTRING sf object with selected columns and correct encoding
 # Crear un objeto sf LINESTRINGS con una selección de columnas y corrección
 # de codificación
-florence_stn = italy_lines %>%
-  st_crop(florence) 
+florence_gpkg = "data/geofabrik_centro-latest.gpkg"
+florence_stn = oe_read(florence_gpkg) %>% 
+  select(name, highway, all_of(extra_tags_ls))
 
 # Save as .rda file
 # Guardar datos como .rda
-save(florence_stn, file = 'data/florence.rda')
+save(florence_stn, file = 'data/florence_stn.rda')
